c54fcf3d13a6653480750bf104bc552ec191ead6
Hunor Liszka <hunor.liszka@protonmail.com>
2024-03-18 10:00:28 +0100

    Migrate package management to Lazy.nvim

---
+++ lua/doublect/lazy/treesitter.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                -- A list of parser names, or "all"
+                ensure_installed = {
+                    "vimdoc", "javascript", "typescript", "c", "cpp", "lua", "rust",
+                    "jsdoc", "bash",
+                },
+
+                sync_install = false,
+                auto_install = true,
+
+                indent = {
+                    enable = true
+                },
+
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = { "markdown" },
+                },
+            })
+        end
+    },
+    {
+        "nvim-treesitter/playground",
+    }
+}
